5ae1cb5f0b08507beb85a6255b4150bcd57c1650
bnicolas <poncec.briannicolas@gmail.com>
2016-09-30 17:34:50 -0300

    SavePoint_1
    Comienzo

---
+++ CPrueba.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+   int i;
+
+   for(i=0; i<20; i++)
+        if (i%2==0)
+            printf("Hola perros\n");
+        else
+            printf("Chau perros\n");
+}
